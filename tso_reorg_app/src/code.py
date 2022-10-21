5a5777bab28d87079d0399835d62aacbf6b76bf3
Rachel Duffin <rachel.g.duffin@gmail.com>
2022-10-21 13:59:28 +0100

    Fix regex, add logfile

--- tso_reorg_app/src/code.py
+++ tso_reorg_app/src/code.py
@@ -1,12 +1,21 @@
-#!/usr/bin/env python
-import json
-import dxpy
-import time
-import re
+#!/usr/bin/env python3
+import json, dxpy, time, re, sys
+from datetime import datetime
+
+def msg_handler(string, logfile_path):
+    """ Dxpy logging seems to interfere with the usual behaviour of python logging module to create a logfile so
+    this function is used as an alternative """
+    message = "{} - {}".format(datetime.now(), string)
+    print(message)
+    with open(logfile_path, "a") as f:
+        f.write("{}\n".format(message))
+
 
 @dxpy.entry_point('main')
 def main(reorg_conf___=None, reorg_status___=None):
     """the outputs of native apps are handled within the app therefore are not included in the custom reorg app"""
+
+    logfile_path = datetime.now().strftime('/home/dnanexus/custom_reorg_%H%M_%d%m%Y.log')
 
     # download and parse `reorg_conf___`
     conf_file = list(dxpy.find_data_objects(
@@ -25,8 +34,6 @@ def main(reorg_conf___=None, reorg_status___=None):
     dxpy.download_dxfile(conf_file[0]['id'], "conf.json")
     with open('conf.json') as f:
         file_types = json.load(f)
-    print("FILE TYPES: ")
-    print(file_types)
 
     # find the output stage of the current analysis
     analysis_id = dxpy.describe(dxpy.JOB_ID)["analysis"]
@@ -34,70 +41,81 @@ def main(reorg_conf___=None, reorg_status___=None):
     # or contains only this reorg job's ID
     while True:
         analysis_desc = dxpy.describe(analysis_id)
-        print("ANALYSIS ID: ")
-        print(analysis_desc)
         depends_on = analysis_desc.get("dependsOn")
-        print("DEPENDS ON: ")
-        print(depends_on)
+        msg_handler("DEPENDS ON: {}".format(depends_on), logfile_path)
         if not depends_on or depends_on == [dxpy.JOB_ID]:
             break
         else:
             time.sleep(3)
 
     stages = analysis_desc["stages"]
-    print("STAGES: ")
-    print(stages)
 
 
     # retrieve the dictionary containing outputs, where key is the name of output and value is the link to the file.
     output_map = [x['execution']['output'] for x in stages if x['id'] == 'stage-outputs'][0]
-    print("OUTPUT MAP: ")
-    print(output_map)
+
     # collect output file IDs - finds all unique file IDS in output map
     out = list(set(re.findall('file-[a-zA-Z0-9]{24}', str(output_map))))
-    print("OUT: ")
-    print(out)
+
     filenames = [dxpy.DXFile(x).describe(fields={'name'}) for x in out]
-    print("FILENAMES: ")
-    print(filenames)
+    msg_handler("COLLECTED FILES: {}".format(filenames), logfile_path)
 
     # Loops through all files and groups files with the same suffix or containing the same string
+    # Removes them from the list of filenames for use by the next step as have already been successfully matched
     # Search mode given as 3 element in conf.json
-    # regex in conf.json may need updating/improving
     files = {}
-    for file in file_types:
+
+    for f_type in file_types:
+        msg_handler('Searching for files of type: {}'.format(f_type), logfile_path)
+        indices = []
+        ids = []
         try:
-            if file_types[file][2] == 'end':
-                files[file] = [x['id'] for x in filenames if x["name"].endswith(file_types[file][1])]
-            elif file_types[file][2] == 'in':
-                files[file] = [x['id'] for x in filenames if file_types[file][1] in x["name"]]
-            elif file_types[file][2] == 'regex':
-                file_ids = []
-                for x in filenames:
+            if file_types[f_type][2] == 'end':
+                # collect ids of files that match so they can be removed from the list of filenames for searching
+                for idx, x in enumerate(filenames):
+                    if x["name"].endswith(file_types[f_type][1]):
+                        ids.append(x['id'])
+                        indices.append(idx)
+                        msg_handler("MATCH for type {}: {}".format(f_type, x), logfile_path)
+            elif file_types[f_type][2] == 'in':
+                # collect ids of files that match so they can be removed from the list of filenames for searching
+                for idx, x in enumerate(filenames):
+                    if file_types[f_type][1] in x["name"]:
+                        ids.append(x['id'])
+                        indices.append(idx)
+                        msg_handler("MATCH for type {}: {}".format(f_type, x), logfile_path)
+            elif file_types[f_type][2] == 'regex':
+                for idx, x in enumerate(filenames):
                     try:
-                        re_result = re.findall(file_types[file][1], x["name"])
+                        re_result = re.findall(file_types[f_type][1], x["name"])
                         if x["name"] == re_result[0]:
-                            file_ids.append(x['id'])
-                    except IndexError as err:
-                        print("regex error:", err, file, x)
+                            ids.append(x['id'])
+                            indices.append(idx)
+                            msg_handler("MATCH for type {}: {}".format(f_type, x), logfile_path)
+                    # there is no regex match
+                    except:
                         pass
-                files[file] = file_ids if len(file_ids) != 0 else print("regex error:", file, x)
+
+            # add file ids to files dictionary and remove filename from filenames list using indices list
+            files[f_type] = ids
+            filenames = [v for i, v in enumerate(filenames) if i not in indices]
+
         except Exception as err:
-            print("file error:", err, file)
+            msg_handler("{}, {}".format(f_type, err), logfile_path)
             pass
-    print("FILES: ")
-    print(files)
+
+    if filenames:
+        msg_handler("NO REGEX MATCH FOR: {}".format(filenames), logfile_path)
+    else:
+        msg_handler("ALL FILES SUCCESSFULLY MATCHED TO DESTINATION FOLDERS: {}".format(files), logfile_path)
+
     # create a dictionary of output folders
     file_folders={}
     for file in files:
         file_folders[file]=file_types[file][0]
-    print("FILE FOLDERS: ")
-    print(file_folders)
 
     # get the container instance
     dx_container = dxpy.DXProject(dxpy.PROJECT_CONTEXT_ID)
-    print("dx_container: ")
-    print(dx_container)
 
     # create folders
     for folder in file_folders:
@@ -105,18 +123,16 @@ def main(reorg_conf___=None, reorg_status___=None):
 
     # move files into folders
     for file_list in files:
+        msg_handler("moving {}, {} to {}".format(file_list, file, file_folders[file_list]), logfile_path)
         try:
             for file in files[file_list]:
-                print("moving", file_list, file, "to", file_folders[file_list])
                 dx_container.move(
                     destination=file_folders[file_list],
                     objects=[file]
                 )
         except Exception as err:
-            print("move error:", err, file_list, file)
+            msg_handler("move error: {}, {}, {}".format(err, file_list, file), logfile_path)
             pass
-    print("OUTPUTS: ")
-    print(dict(outputs=output_map))
 
     # create array to populate 'outputs' in OutputSpec in dxapp.json
     out_array=[]
@@ -124,6 +140,9 @@ def main(reorg_conf___=None, reorg_status___=None):
         file_dict = {'$dnanexus_link': item}
         out_array.append(file_dict)
 
-    print(dict(outputs=out_array))
+    # Specify the outputs
+    output= {"logfile": dxpy.dxlink(dxpy.upload_local_file(logfile_path)), "outputs": out_array}
 
-    return dict(outputs=out_array)
+    return output
+
+dxpy.run()
